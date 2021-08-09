4e99d4d6357a29e4aedb0a679e46bcd5216c3f8d
Praveen Prajapati <praveen.prajapati.rebase@gmail.com>
2021-08-09 15:11:27 +0530

    developed recursive program to add nodes

---
+++ Day 2 Recursive-Async/asyncFunction.js
@@ -0,0 +1,12 @@
+
+const makeCoffie = new Promise ((resolve, reject) => {
+
+    if (milk===true) {
+        if (suger===true) {
+            resolve("Can make it")
+        }
+        else {
+            resolve("Can make it for ")
+        }
+    }
+})
