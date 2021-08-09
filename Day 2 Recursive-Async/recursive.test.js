4742076ead8271f48980bd526274c7cbd3664dfe
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2021-08-09 18:22:37 +0530

    Praveen/rexursive training (#4)
    * developed recursive program to add nodes
    
    * Day 2

---
+++ Day 2 Recursive-Async/recursive.test.js
@@ -0,0 +1,50 @@
+// const createNode = require('./recursive')
+
+const recursive = require('./recursive')
+const assert = require ('assert')
+
+let NODE = {
+    name:"A",
+    description: "root",
+    children: []
+}
+
+describe( "Creating the NODE", () => {  
+    beforeEach(
+        NODE= {
+            name: "A",
+            description: "root",
+            children: []
+    })
+
+    describe("createNode", () => {
+        tests = [
+            {
+                description: "S",
+                expected: true,
+                query: recursive.createNode(NODE, "B", "node 1", "A")
+            },
+            {
+                description: "Eliminate node without name entry",
+                expected: false,
+                query: recursive.createNode(NODE, "", "node 1", "A")
+            },
+            {
+                description: "Reject node already present",
+                expected: false,
+                query: recursive.createNode(NODE, "B", "node 1", "A")
+            },
+            {
+                description: "Make description compolsory",
+                expected: false,
+                query: recursive.createNode(NODE, "C", "", "A")
+            }
+        ],
+        tests.forEach(test => {
+            it(test.description, () => {
+              assert.equal(test.query , test.expected);
+            });   
+        });
+    }   )
+     
+});
